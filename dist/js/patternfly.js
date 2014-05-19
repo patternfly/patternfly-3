bf6b9f60c5dcab7e69c92202d043d99534126790
Robb Hamilton <rhamilto@redhat.com>
2014-05-19 16:00:24 -0400

    Enclosing sidebar and search js in immediately invoked function expressions to avoid conflicts

--- dist/js/patternfly.js
+++ dist/js/patternfly.js
@@ -1,34 +1,33 @@
 // PatternFly Namespace
 var PatternFly = PatternFly || {};
 
-$(document).ready(function() {
-  // Call sidebar() on ready if .sidebar-pf exists and .datatable does not exist
-  if ($('.sidebar-pf').length > 0 && $('.datatable').length == 0) {
-  //if ($('.sidebar-pf').length > 0) {
-    sidebar();
-  }
-});
-
-$(window).resize(function() {
-  // Call sidebar() on resize if .sidebar-pf exists
-  if ($('.sidebar-pf').length > 0) {
-    sidebar();
-  }
-});
-
-// PatternFly Sidebar 
+// Util: PatternFly Sidebar 
 // Set height of sidebar-pf to height of document minus height of navbar-pf if not mobile
-function sidebar() {
-  var documentHeight = 0;
-  var navbarpfHeight = 0;
-  var colHeight = 0;
-  if ( $('.navbar-pf .navbar-toggle').is(':hidden') ) {
-    documentHeight = $(document).height();
-    navbarpfHeight = $('.navbar-pf').outerHeight();
-    colHeight = documentHeight - navbarpfHeight;
+(function($) {
+  sidebar = function() {
+    var documentHeight = 0;
+    var navbarpfHeight = 0;
+    var colHeight = 0;
+    if ( $('.navbar-pf .navbar-toggle').is(':hidden') ) {
+      documentHeight = $(document).height();
+      navbarpfHeight = $('.navbar-pf').outerHeight();
+      colHeight = documentHeight - navbarpfHeight;
+    }
+    $('.sidebar-pf').parent('.row').children('[class*="col-"]').css({ "min-height":colHeight});
   }
-  $('.sidebar-pf').parent('.row').children('[class*="col-"]').css({ "min-height":colHeight});
-}
+  $(document).ready(function() {
+    // Call sidebar() on ready if .sidebar-pf exists and .datatable does not exist
+    if ($('.sidebar-pf').length > 0 && $('.datatable').length == 0) {
+      sidebar();
+    }
+  });
+  $(window).resize(function() {
+    // Call sidebar() on resize if .sidebar-pf exists
+    if ($('.sidebar-pf').length > 0) {
+      sidebar();
+    }
+  });
+})(jQuery);
 
 // Util: PatternFly Popovers
 // Add data-close="true" to insert close X icon
