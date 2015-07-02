450286161f65c65184e84c14e4f1d7ccea8620a6
Alik Kurdyukov <alik@kurdyukov.com>
2015-07-02 09:51:41 +0300

    popovers fixed

--- dist/js/patternfly.js
+++ dist/js/patternfly.js
@@ -34,17 +34,13 @@
 // Add data-close="true" to insert close X icon
 (function ($) {
   'use strict';
-  // PatternFly Namespace
-  var PatternFly = window.PatternFly || {};
 
-  PatternFly.popovers = function (selector) {
-    var allpopovers = $(selector);
-
+  $.fn.popovers = function () {
     // Initialize
-    allpopovers.popover();
+    this.popover();
 
     // Add close icons
-    allpopovers.filter('[data-close=true]').each(function (index, element) {
+    this.filter('[data-close=true]').each(function (index, element) {
       var $this = $(element),
         title = $this.attr('data-original-title') + '<button type="button" class="close" aria-hidden="true"><span class="pficon pficon-close"></span></button>';
 
@@ -52,7 +48,7 @@
     });
 
     // Bind Close Icon to Toggle Display
-    allpopovers.on('click', function (e) {
+    this.on('click', function (e) {
       var $this = $(this),
         $title = $this.next('.popover').find('.popover-title');
 
@@ -67,6 +63,8 @@
       // Prevent href="#" page scroll to top
       e.preventDefault();
     });
+
+    return this;
   };
 }(jQuery));
 
