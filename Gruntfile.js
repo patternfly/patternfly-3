43bd21aba5d0c20cc513e6695d0044200d1cbcf2
Sarah Rambacher <srambach@redhat.com>
2016-09-02 16:27:56 -0400

    Eliminates copying node_modules less files into PF less directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,22 +42,6 @@ module.exports = function (grunt) {
           {expand: true, cwd: 'node_modules/font-awesome/fonts/', src: ['*'], dest: 'dist/fonts/'},
           // copy Patternfly font files
           {expand: true, cwd: 'src/fonts/', src: ['*'], dest: 'dist/fonts/'},
-          // copy Bootstrap less files
-          {expand: true, cwd: 'node_modules/bootstrap/less/', src: ['**'], dest: 'src/less/lib/bootstrap/'},
-          // copy Font Awesome less files
-          {expand: true, cwd: 'node_modules/font-awesome/less/', src: ['**'], dest: 'src/less/lib/font-awesome/'},
-          // copy Bootstrap-Combobox less files
-          {expand: true, cwd: 'node_modules/patternfly-bootstrap-combobox/less/', src: ['**'], dest: 'src/less/lib/bootstrap-combobox/'},
-          // copy Bootstrap-Datepicker less files
-          {expand: true, cwd: 'node_modules/bootstrap-datepicker/less/', src: ['**'], dest: 'src/less/lib/bootstrap-datepicker/'},
-          // copy Bootstrap-Select less files
-          {expand: true, cwd: 'node_modules/bootstrap-select/less/', src: ['**'], dest: 'src/less/lib/bootstrap-select/'},
-          // Bootstrap Switch less files must be manually copied because of edits made to source less for strict-math purposes
-          // manually copy 'node_modules/bootstrap-switch/src/less/bootstrap3/' and make sure any math is wrapped with parentheses
-          // copy Bootstrap Touchspin css file
-          {expand: true, cwd: 'node_modules/bootstrap-touchspin/dist/', src: ['jquery.bootstrap-touchspin.css'], dest: 'src/less/lib/bootstrap-touchspin/'},
-          // copy C3 css file
-          {expand: true, cwd: 'node_modules/c3/', src: ['c3.css'], dest: 'src/less/lib/c3/'},
           //copy images
           {expand: true, cwd: 'src/img/', src: ['**'], dest: 'dist/img/'}
         ],
@@ -109,7 +93,10 @@ module.exports = function (grunt) {
           'dist/css/patternfly.css': 'src/less/patternfly.less',
         },
         options: {
-          paths: ['src/less/'],
+          paths: [
+            'src/less/',
+            'node_modules/'
+          ],
           strictMath: true,
           sourceMap: true,
           outputSourceFiles: true,
@@ -122,7 +109,10 @@ module.exports = function (grunt) {
           'dist/css/patternfly-additions.css': 'src/less/patternfly-additions.less'
         },
         options: {
-          paths: ['src/less/'],
+          paths: [
+            'src/less/',
+            'node_modules/'
+          ],
           strictMath: true,
           sourceMap: true,
           outputSourceFiles: true,
@@ -146,13 +136,6 @@ module.exports = function (grunt) {
         files: [
           'node_modules/bootstrap/dist/fonts/**/*',
           'node_modules/font-awesome/fonts/**/*',
-          'node_modules/bootstrap/less/**/*',
-          'node_modules/font-awesome/less/**/*',
-          'node_modules/patternfly-bootstrap-combobox/less/**/*',
-          'node_modules/bootstrap-datepicker/less/**/*',
-          'node_modules/bootstrap-select/less/**/*',
-          'node_modules/bootstrap-touchspin/dist/**/*',
-          'node_modules/c3/**/*',
           'src/fonts/**/*',
           'src/img/**/*'
         ],
@@ -163,7 +146,16 @@ module.exports = function (grunt) {
         tasks: ['jekyll']
       },
       less: {
-        files: 'src/less/*.less',
+        files: [
+          'src/less/*.less',
+          'node_modules/bootstrap/less/**/*',
+          'node_modules/font-awesome/less/**/*',
+          'node_modules/patternfly-bootstrap-combobox/less/**/*',
+          'node_modules/bootstrap-datepicker/less/**/*',
+          'node_modules/bootstrap-select/less/**/*',
+          'node_modules/bootstrap-touchspin/dist/**/*',
+          'node_modules/c3/**/*'
+        ],
         tasks: ['less']
       },
       css: {
