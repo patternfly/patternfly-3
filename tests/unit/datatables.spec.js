34a032c9550dfe5a1d2f0298d2e7e4978827a49f
Dan <dlabrecq@redhat.com>
2016-09-08 17:45:54 -0400

    PTNFLY-877 Table View Implementation. This introduces functionality for row selection, filtering, and an empty data table.

--- tests/unit/datatables.spec.js
+++ tests/unit/datatables.spec.js
@@ -4,19 +4,21 @@ describe("data tables test suite", function () {
     globals.readFixture(globals.fixturePath + 'datatables.html');
 
     //run the plugin before each test
-    $('.datatable').dataTable();
+    $('.datatable-pf table').dataTable();
   });
 
   it('should render a table with striped rows and borders', function (done) {
-    var table = $('.datatable');
+    var table = $('.datatable-pf table');
 
     setTimeout(function () {
+      expect(table).toHaveClass('table');
       expect(table).toHaveClass('table-striped');
       expect(table).toHaveClass('table-bordered');
+      expect(table).toHaveClass('table-hover');
       done();
     }, globals.wait);
   });
-
+/** Pagination is not currently implemented
   it('should go to page two after clicking next', function (done) {
     var pager = $('.dataTables_paginate ul li.next');
     var page = $('.dataTables_footer .pagination-input input');
@@ -28,5 +30,5 @@ describe("data tables test suite", function () {
       done();
     }, globals.wait);
   });
-
+*/
 });
