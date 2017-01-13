f12fde2dd924ef4664fbb04972ec16d5eda406c3
Marco <diener.marco@gmail.com>
2017-01-13 12:46:59 +0000

    Fix bootstrap_input fnInit for negative iNewStart

--- src/js/patternfly-functions.js
+++ src/js/patternfly-functions.js
@@ -171,7 +171,7 @@
             }
 
             iNewStart = oSettings._iDisplayLength * (this.value - 1);
-            if (iNewStart >= oSettings.fnRecordsDisplay()) {
+            if (iNewStart >= oSettings.fnRecordsDisplay() || iNewStart < 0) {
               /* Display overrun */
               oSettings._iDisplayStart = (Math.ceil(oSettings.fnRecordsDisplay() /
                 oSettings._iDisplayLength) - 1) * oSettings._iDisplayLength;
