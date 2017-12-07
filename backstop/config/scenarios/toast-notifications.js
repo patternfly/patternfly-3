bf3e797f4832542a82870fcdc52298b89415b029
Cliff Pyles <cpyles@redhat.com>
2017-12-07 16:45:35 -0500

    chore(tests): Fix toast-notification regressions

---
+++ backstop/config/scenarios/toast-notifications.js
@@ -0,0 +1,9 @@
+module.exports = [{
+  removeSelectors: ['.container > .alert'],
+  selectors: ['.toast-notifications-list-pf'],
+  selectorExpansion: true,
+  label: 'toasts',
+  url: 'dist/tests/toast.html',
+  disabled: false,
+  delay: 2000
+}]
--- backstop/config/scenarios/toasts.js
+++
@@ -1,6 +0,0 @@
-module.exports = [{
-  removeSelectors: ['.page-header + .alert'],
-  label: 'toasts',
-  url: 'dist/tests/toasts.html',
-  disabled: true
-}]
