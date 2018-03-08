98cf0847d1346cac009e1b6cff43dd032d2aa04e
patternfly-build <patternfly-build@redhat.com>
2018-03-08 16:59:59 +0000

    chore(release): updates generated by Travis build

--- dist/js/patternfly-settings-base.js
+++ dist/js/patternfly-settings-base.js
@@ -2,7 +2,7 @@
   'use strict';
 
   var patternfly = {
-    version: "0.0.0-semantically-released",
+    version: "3.41.5"
   };
 
   // definition of breakpoint sizes for tablet and desktop modes
--- src/js/patternfly-settings-base.js
+++ src/js/patternfly-settings-base.js
@@ -2,7 +2,7 @@
   'use strict';
 
   var patternfly = {
-    version: "3.41.5"
+    version: "3.41.6"
   };
 
   // definition of breakpoint sizes for tablet and desktop modes
