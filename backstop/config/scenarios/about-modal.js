8cabd2f7ce0c769fb02b3c942be054f02ae46f0a
Xuebin Dong <dabeng413@gmail.com>
2018-03-22 21:09:55 +0800

    chore(test): Setup initial prep for the about-modal regression test (#994)

--- backstop/config/scenarios/about-modal.js
+++ backstop/config/scenarios/about-modal.js
@@ -3,4 +3,10 @@ module.exports = [{
   label: 'about-modal',
   url: 'dist/tests/about-modal.html',
   disabled: false
+}, {
+  label: 'about-modal-show-modal',
+  url: 'dist/tests/about-modal.html',
+  removeSelectors: ['.page-header + .alert'],
+  clickSelector: '[data-target="#about-modal-1"]',
+  selectors: ['body']
 }]
