9039b54bf3486b5b174477eea32e4f6ed388a4d8
Xuebin Dong <dabeng413@gmail.com>
2018-03-28 20:52:17 +0800

    chore(tests): Setup initial prep for the wizard regression test (#1003)

--- backstop/config/scenarios/wizard.js
+++ backstop/config/scenarios/wizard.js
@@ -3,4 +3,23 @@ module.exports = [{
   label: 'wizard',
   url: 'dist/tests/wizard.html',
   disabled: false
+}, {
+  label: 'wizard-first-step',
+  url: 'dist/tests/wizard.html',
+  removeSelectors: ['.page-header + .alert'],
+  selectors: ['body'],
+  clickSelector: '[data-target="#complete"]',
+  postInteractionWait: 3000
+}, {
+  label: 'wizard-initializing',
+  url: 'dist/tests/wizard.html',
+  removeSelectors: ['.page-header + .alert'],
+  selectors: ['body'],
+  clickSelector: '[data-target="#initializing"]'
+}, {
+  label: 'wizard-last-step',
+  url: 'dist/tests/wizard.html',
+  removeSelectors: ['.page-header + .alert'],
+  selectors: ['body'],
+  clickSelector: '[data-target="#done"]'
 }]
