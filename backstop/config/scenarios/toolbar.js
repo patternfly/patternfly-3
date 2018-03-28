b146c9cf7cdad0946556c70f7aff2be7ec472a45
Xuebin Dong <dabeng413@gmail.com>
2018-03-28 20:51:54 +0800

    chore(tests): Setup initial prep for toolbar regression test (#1000)

--- backstop/config/scenarios/toolbar.js
+++ backstop/config/scenarios/toolbar.js
@@ -3,4 +3,24 @@ module.exports = [{
   label: 'toolbar',
   url: 'dist/tests/toolbar.html',
   disabled: false
+}, {
+  label: 'toolbar-input-filter-button',
+  url: 'dist/tests/toolbar.html',
+  removeSelectors: ['.page-header + .alert'],
+  clickSelector: '#input-filter-button'
+}, {
+  label: 'toolbar-filter-button',
+  url: 'dist/tests/toolbar.html',
+  removeSelectors: ['.page-header + .alert'],
+  clickSelector: '#filter-button'
+}, {
+  label: 'toolbar-dropdownKebab',
+  url: 'dist/tests/toolbar.html',
+  removeSelectors: ['.page-header + .alert'],
+  clickSelector: '#dropdownKebab'
+}, {
+  label: 'toolbar-find-button',
+  url: 'dist/tests/toolbar.html',
+  removeSelectors: ['.page-header + .alert'],
+  clickSelector: '.btn-find'
 }]
