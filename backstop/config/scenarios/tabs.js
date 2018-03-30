ec3e90f2bae39b309ea8004d4eba5739b5356d0c
dabeng <dabeng@localhost.localdomain>
2018-03-30 09:18:16 +0800

    chore(tests): Setup initial prep for the tabs regression test

--- backstop/config/scenarios/tabs.js
+++ backstop/config/scenarios/tabs.js
@@ -3,4 +3,14 @@ module.exports = [{
   label: 'tabs',
   url: 'dist/tests/tabs.html',
   disabled: false
+}, {
+  label: 'tabs-with-dropdown',
+  url: 'dist/tests/tabs.html',
+  onReadyScript: 'chromy/tabs.js',
+  selectors: ['#tabs-with-dropdown-wrapper']
+}, {
+  label: 'tabs-pf-with-dropdown',
+  url: 'dist/tests/tabs.html',
+  onReadyScript: 'chromy/tabs.js',
+  selectors: ['#tabs-pf-with-dropdown-wrapper']
 }]
