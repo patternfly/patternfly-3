61dc0d13cd5e9203f04c551afaaed52cadc7c24d
Jeffrey Phillips <jephilli@redhat.com>
2018-08-15 10:19:40 -0400

    feat(flexbox layout): Add flexbox layout w/ example pages

--- backstop/config/scenarios/vertical-navigation.js
+++ backstop/config/scenarios/vertical-navigation.js
@@ -1,32 +1,50 @@
 module.exports = [
   {
-    removeSelectors: ['.page-header + .alert'],
+    removeSelectors: ['.toast-pf'],
     label: 'vertical-navigation-primary-only',
     url: 'dist/tests/vertical-navigation-primary-only.html',
     disabled: false
   },
   {
-    removeSelectors: ['.page-header + .alert'],
+    removeSelectors: ['.toast-pf'],
     label: 'vertical-navigation-with-badges',
     url: 'dist/tests/vertical-navigation-with-badges.html',
     disabled: false
   },
   {
-    removeSelectors: ['.page-header + .alert'],
+    removeSelectors: ['.toast-pf'],
     label: 'vertical-navigation-with-secondary',
     url: 'dist/tests/vertical-navigation-with-secondary.html',
     disabled: false
   },
   {
-    removeSelectors: ['.page-header + .alert'],
+    removeSelectors: ['.toast-pf'],
     label: 'vertical-navigation-with-tertiary-no-icons',
     url: 'dist/tests/vertical-navigation-with-tertiary-no-icons.html',
     disabled: false
   },
   {
-    removeSelectors: ['.page-header + .alert'],
+    removeSelectors: ['.toast-pf'],
     label: 'vertical-navigation-with-tertiary-pins',
     url: 'dist/tests/vertical-navigation-with-tertiary-pins.html',
+    disabled: false
+  },
+  {
+    removeSelectors: ['.toast-pf'],
+    label: 'vertical-navigation-with-sidebar',
+    url: 'dist/tests/vertical-navigation-with-sidebar.html',
+    disabled: false
+  },
+  {
+    removeSelectors: ['.toast-pf'],
+    label: 'vertical-navigation-flexbox',
+    url: 'dist/tests/vertical-navigation-flexbox.html',
+    disabled: false
+  },
+  {
+    removeSelectors: ['.toast-pf'],
+    label: 'vertical-navigation-flexbox-inner-content',
+    url: 'dist/tests/vertical-navigation-flexbox-inner-content.html',
     disabled: false
   }
 ]
